ae75ffa2adbbb5b0a17532a3ff50c336e5785989
SouptikTaran <mr.souptiktRn@gmail.com>
2025-03-21 15:01:31 +0530

    navbar made

--- src/pages/Astradev.tsx
+++ src/pages/Astradev.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
 import bg from '../assets/images/bg.png'
-import IntroBox from '../components/IntroBox';
 import { SparklesCore } from '@/components/ui/sparkles';
 import { Typewriter } from '@/components/ui/typewriter-text';
-import WebgiViewer from '@/components/WebgiViewer';
-import { Timeline } from '@/components/ui/timeline';
-import { TimelineDemo } from '@/components/Things_we_do';
+import Navbar from "@/components/Navbar"
+
 
 const Astradev = () => {
   return (
     <div className=' h-auto w-full bg-[#08021E] '>
+        <Navbar />
       <img src={bg} className='w-full absolute' />
-
-    
-
       <div className="h-[45rem] w-full bg-[#08021E] flex flex-col items-center justify-center overflow-hidden rounded-md">
 
         <div className="h-[40rem] relative w-full  flex flex-col items-center justify-center overflow-hidden rounded-md">
@@ -24,15 +19,15 @@ const Astradev = () => {
               minSize={0.6}
               maxSize={1.8}
               particleDensity={80}
-              className="w-full h-[60rem]"
+              className="w-screen h-screen"
               particleColor="#FFFFFF"
               speed={1}
             />
           </div>
-          <h1 className="md:text-7xl top-[5rem] text-3xl lg:text-6xl font-Josefin_Sans font-bold text-center text-white relative z-20">
-            Welcome To Astradev
-          </h1>
-          <div className='top-[30rem] absolute'>
+          <h1 className="text-9xl md:text-8xl font-Josefin_Sans font-bold text-center text-white relative z-20 uppercase">
+          Astradev
+        </h1>
+          <div className='top-[25rem] absolute'>
             <Typewriter
               text={["Build  .  Develop   .   Deploy", "Build  .  Develop   .   Deploy", "Build  .  Develop   .   Deploy"]}
               speed={50}
@@ -44,22 +39,10 @@ const Astradev = () => {
 
       </div>
 
-      <div className='absolute top-[40rem] flex justify-evenly gap-5  w-full h-[90rem]'>
-        <div className='w-1/2'>
-        <TimelineDemo/>
-        </div>
 
-        <div className='w-1/2' >
-        <IntroBox />
-        </div>
-      </div>
-      <div id='start-trigger'/>
-      <div className='h-[50rem]  w-full relative z-50 border flex justify-center items-center top-[60rem]' >
-      <WebgiViewer/>
-      </div>
     </div>
   
   )
 }
 
-export default Astradev
+export default Astradev 
